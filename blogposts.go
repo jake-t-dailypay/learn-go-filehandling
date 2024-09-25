8cdd77ebc3b5c8a9945cb3f5fad0140e88b51922
jake-t-dailypay <jake.turner@dailypay.com>
2024-09-25 15:22:13 +0100

    file parsing

--- blogposts.go
+++ blogposts.go
@@ -1,10 +1,20 @@
 package blogposts
 
 import (
+	"errors"
+	"io"
 	"io/fs"
 )
 
 type Post struct {
+	Title string
+}
+
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh no, i always fail")
 }
 
 func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
@@ -13,8 +23,31 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 		return nil, err
 	}
 	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f)
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
 	}
 	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return newPost(postFile)
+}
+
+func newPost(postFile fs.File) (Post, error) {
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	post := Post{Title: string(postData)[7:]}
+	return post, nil
 }
